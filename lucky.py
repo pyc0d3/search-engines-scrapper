593c7c68b9fd207d8dc316a06e2cf2e9eac22e41
Vadim Yashigulov <pyc0d3@gmail.com>
2020-04-22 17:26:20 +0300

    Added Yandex scrapper. Refactoring

--- lucky.py
+++ lucky.py
@@ -4,19 +4,14 @@ import requests
 import argparse
 from fake_useragent import UserAgent
 
-ua = UserAgent()
-headers = {
-    'User-Agent': ua.random,
-}
-
 
 class Parser(argparse.ArgumentParser):
     def __init__(self):
         super().__init__(self)
 
         self.add_argument('--request', '-r', default='kitties pics')
-        self.add_argument('--search_engine', '-se', default='google')
-        self.add_argument('--number', '-n', default=3, type=int)
+        self.add_argument('--search_engine', '-se', default='yandex')
+        self.add_argument('--number', '-n', default=5, type=int)
 
     def get_engine(self):
         return self.parse_args().search_engine
@@ -39,28 +34,39 @@ class Scrapper:
             raise Exception('Engine is not valid, try another!')
 
         self.engine = self.search_engines[engine]
+        self.ua = UserAgent()
+        self.headers = {
+            'User-Agent': self.ua.random,
+        }
 
     def _get_from_google(self):
         """Находит на (1) странице запроса все ссылки и вовращает их"""
         page = requests.get(
-            'https://www.google.com/search?q=' + self.request, headers=headers)
+            f'https://www.google.com/search?q={self.request}', headers=self.headers)
         soup = bs4.BeautifulSoup(page.content, 'html.parser')
         return [div.find('a')['href'] for div in soup.find_all('div', class_='r')]
 
     def _get_from_yandex(self):
-        # TODO: сделать скрапер яндекса
-        pass
+        """Находит на (1) странице запроса все ссылки и вовращает их"""
+        page = requests.get(
+            f'https://yandex.ru/search/?lr=10000&text={self.request}', headers=self.headers)
+        soup = bs4.BeautifulSoup(page.content, 'html.parser')
+        return [a['href'] for a in soup.find_all('a', class_=['organic__url', 'OrganicTitle-Link'])]
 
     def get_urls(self, num):
         """Возвращает первые num или меньше ссылок"""
-        print(num)
         return self.engine()[: min(len(self.engine()), num)]
 
 
-cmd = Parser()
-scr = Scrapper(cmd.get_engine(), cmd.get_request())
-print('Гуглим...')
-for url in scr.get_urls(cmd.get_url_number()):
-    print(url)
-    webbrowser.open_new_tab(url)
-print('Готово!')
+def main():
+    cmd = Parser()
+    scr = Scrapper(cmd.get_engine(), cmd.get_request())
+    print(f'Гуглим с помощью {cmd.get_engine()}')
+    for url in scr.get_urls(cmd.get_url_number()):
+        print(url)
+        webbrowser.open_new_tab(url)
+    print('Готово!')
+
+
+if __name__ == '__main__':
+    main()
